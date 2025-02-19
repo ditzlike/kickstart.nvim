6e9dabd5178085edd12a44e3c9a1ebfa1b6343a2
Pia Eichinger <pia.eichinger@vector.com>
2025-02-19 12:46:50 +0100

    comment out everything, clean some old comments

--- init.lua
+++ init.lua
@@ -1,52 +1,6 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -60,12 +14,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
@@ -74,16 +22,17 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   --'tpope/vim-sleuth',
 
-  require 'plugins.autocomplete',
+  --require 'plugins.autocomplete',
 
 
   --TODO
-  require 'plugins.whichkey',
+  --require 'plugins.whichkey',
+  require 'plugins.lsp',
 
   --TODO
   --require 'plugins.git-signs',
 
-  require 'plugins.theme',
+  --require 'plugins.theme',
 
   --TODO
   --require '.plugins.lualine',
@@ -110,7 +59,7 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  require 'plugins.autoformat',
+  --require 'plugins.autoformat',
   --require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -121,9 +70,9 @@ require('lazy').setup({
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
   -- this is where I put some personal plugins
-  require 'custom.plugins.general',
-  require 'custom.plugins.obsidian',
-  require 'custom.plugins.toggleterm'
+  --require 'custom.plugins.general',
+  --require 'custom.plugins.obsidian',
+  --require 'custom.plugins.toggleterm'
 }, {})
 
 --TODO
@@ -133,23 +82,23 @@ require('lazy').setup({
 -- my own mappings :>
 require 'mappings'
 require 'settings'
+require 'style'
 --TODO
 --require 'configs.mappings'
 
-require 'plugins.telescope'
+--require 'plugins.telescope'
 
-require 'plugins.treesitter'
+--require 'plugins.treesitter'
 
 -- these two go together
-require 'plugins.lsp'
-require 'plugins.nvim-cmp'
+--require 'plugins.nvim-cmp'
 
 
 -- [[ My own stuff]]
-require 'custom.configs.harpoon'
-require 'custom.configs.nvim-tree'
-require 'custom.configs.toggleterm'
-require 'custom.configs.obisidian'
+--require 'custom.configs.harpoon'
+--require 'custom.configs.nvim-tree'
+--require 'custom.configs.toggleterm'
+--require 'custom.configs.obisidian'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
