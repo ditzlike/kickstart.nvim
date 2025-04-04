a120361751e1363ccf29dbb91507e10a16a7e4f8
Pia Eichinger <pia.eichinger@vector.com>
2025-04-04 14:32:23 +0200

    test: add linter.lua and catppuccin and remove lsp.lua

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,49 @@
+return {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+        vim.cmd.colorscheme('catppuccin')
+    end,
+    opts = {
+        flavour = 'mocha',
+        integrations = {
+            blink_cmp = true,
+            dap = true,
+            dap_ui = true,
+            flash = true,
+            gitsigns = true,
+            mason = true,
+            native_lsp = {
+                enabled = true,
+                underlines = {
+                    errors = { 'undercurl' },
+                    warnings = { 'undercurl' },
+                    hints = { 'underline' },
+                    information = { 'underline' },
+                },
+            },
+            neotree = true,
+            neogit = true,
+            notify = true,
+            overseer = true,
+            rainbow_delimiters = true,
+            semantic_tokens = true,
+            snacks = { enabled = true },
+            telescope = true,
+            treesitter = true,
+            treesitter_context = true,
+        },
+    },
+    specs = {
+        {
+            'akinsho/bufferline.nvim',
+            optional = true,
+            opts = function(_, opts)
+                if (vim.g.colors_name or ''):find('catppuccin') then
+                    opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
+                end
+            end,
+        },
+    },
+}
---
+++ lua/plugins/linter.lua
@@ -0,0 +1,6 @@
+return {
+    'nvimtools/none-ls.nvim',
+    event = 'VeryLazy',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true
+}
